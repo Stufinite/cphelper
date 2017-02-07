72d24c8d7204a27ed27188b6f54cf9758e25d312
davidtnfsh <davidtnfsh@gmail.com>
2017-02-07 13:04:47 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name = 'cphelper',
     packages = ['cphelper'],
-    version = '1.1',
+    version = '1.4',
     description = 'A API which will return Course of specific Dept. and also Course which you can enroll at that time.',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/Stufinite/cphelper',
-    download_url = 'https://github.com/Stufinite/cphelper/archive/v1.1.tar.gz',
+    download_url = 'https://github.com/Stufinite/cphelper/archive/v1.4.tar.gz',
     keywords = ['coursepickinghelper', 'timetable', 'campass'],
     classifiers = [],
     license='GNU3.0',
     install_requires=[
-        'djangoApiDec==1.2',
+        'djangoApiDec',
         'pymongo==3.4.0',
     ],
     zip_safe=True
